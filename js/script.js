43d8eb1151502c7f72a20968433f948efb219efb
Mostafa Dashti <mo_dashti1@yahoo.com>
2015-03-05 13:29:36 +0330

    typing problem

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
 
 	$('.mouse-move').click(function (){
@@ -7,14 +8,14 @@ $(document).ready(function(){
 
 
 	$(window).resize(function() {
-		var pageWidth = $(window).width(); 
+		var pageWidth = $(window).width();
 		if( $(window).width()< 768){
 			$('.large-desk').addClass('visibility-hidden');
 			$('.mobile-size').removeClass('visibility-hidden');
 		}
 	});
-	
-	
+
+
 		$('.navbar-toggle').click(function() {
 			$('.navbar-toggle').fadeOut();
 			$('.menu').addClass('opened');
@@ -22,44 +23,73 @@ $(document).ready(function(){
 			$('.menu').css('height',height);
 
 	});
-	
+
 	$('.btnClose').click(function() {
 			$('.menu').removeClass('opened');
 			$('.navbar-toggle').fadeIn();
 
 	});
 
-	var text=[];
-	text[0]='تهران';
-	text[1]='مشهد';
-	text[2]='اصفهان';
-	text[3]='تبریز';
-	text[4]='کرج';
-	text[5]='آمل';
-	text[6]='اراک';
-	text[7]='اردبیل';
-	text[8]='ارومیه';
-	text[9]='اهواز';
-	text[10]='بابل';
-	text[11]='زنجان';
-	text[12]='شیراز';
-	text[13]='کرمان';
-	text[14]='کرمانشاه';
 
-	for (var j=0; j<text.length; j++) {
-		var chars = text[j].slice('');
-		var container = document.getElementById("write");
-		var i = 0;
-		setInterval(function () {
-    	if (i < chars.length) {
-       	 container.innerHTML += chars[i++];
-    	} else {
-        	i = 0;
-        	container.innerHTML = "";
-   		 }
-		}, 800);
-	};
-	
+
+	//var str = "<p>This is my <span style='color:red;'>special string</span> with an <img src='http://placehold.it/150x150'> image !</p><p>This is my <span style='color:red;'>special string</span> with an <img src='http://placehold.it/150x150'> image !</p>",
+	var i = 0,
+	isTag,
+	text;
+	var city_id = 0;
+	(function type() {
+		var text_arr= new Array();
+		text_arr[0]='تهران;';
+		text_arr[1]='مشهد;';
+		text_arr[2]='اصفهان;';
+		text_arr[3]='تبریز;';
+		text_arr[4]='کرج;';
+		text_arr[5]='آمل;';
+		text_arr[6]='اراک;';
+		text_arr[7]='اردبیل;';
+		text_arr[8]='ارومیه;';
+		text_arr[9]='اهواز;';
+		text_arr[10]='بابل;';
+		text_arr[11]='زنجان;';
+		text_arr[12]='شیراز;';
+		text_arr[13]='کرمان;';
+		text_arr[14]='کرمانشاه;';
+
+		var str = text_arr[city_id];
+		text = str.slice(0, ++i);
+		if (text === str)
+		{
+			city_id++;
+			i = 0;
+			if(city_id > 14)
+				city_id = 0
+				return type();
+		}
+
+		document.getElementById("write").innerHTML = text;
+
+		var char = text.slice(-1);
+		if( char === '<' ) isTag = true;
+		if( char === '>' ) isTag = false;
+
+		if (isTag) return type();
+		setTimeout(type, 600);
+	}());
+
+
+	// for (var j=0; j<text.length; j++) {
+	// 	var chars = text[j].slice('');
+	// 	var container = document.getElementById("write");
+	// 	var i = 0;
+	// 	setInterval(function () {
+    // 	if (i < chars.length) {
+    //    	 container.innerHTML += chars[i++];
+    // 	} else {
+    //     	i = 0;
+    //     	container.innerHTML = "";
+ //   		 }
+	// 	}, 220);
+	// };
+
 
 });
-
