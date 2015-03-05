8a8835ec6d8411c9602e898120aa6d7cdfaef581
kafiyan <maedeh.kafiyan@gmail.com>
2015-03-05 12:46:43 +0330

    update fixing bugs

--- js/script.js
+++ js/script.js
@@ -29,22 +29,36 @@ $(document).ready(function(){
 
 	});
 
-	// var text=[];
-	// text[0]='تهران';
-	// text[1]='مشهد';
-	// for (var j=0; j<text.length; j++) {
-	// 	var chars = text[j].slice('');
-	// 	var container = document.getElementById("write");
-	// 	var i = 0;
-	// 	setInterval(function () {
- //    	if (i < chars.length) {
- //       	 container.innerHTML += chars[i++];
- //    	} else {
- //        	i = 0;
- //        	container.innerHTML = "";
- //   		 }
-	// 	}, 800);
-	// };
+	var text=[];
+	text[0]='تهران';
+	text[1]='مشهد';
+	text[2]='اصفهان';
+	text[3]='تبریز';
+	text[4]='کرج';
+	text[5]='آمل';
+	text[6]='اراک';
+	text[7]='اردبیل';
+	text[8]='ارومیه';
+	text[9]='اهواز';
+	text[10]='بابل';
+	text[11]='زنجان';
+	text[12]='شیراز';
+	text[13]='کرمان';
+	text[14]='کرمانشاه';
+
+	for (var j=0; j<text.length; j++) {
+		var chars = text[j].slice('');
+		var container = document.getElementById("write");
+		var i = 0;
+		setInterval(function () {
+    	if (i < chars.length) {
+       	 container.innerHTML += chars[i++];
+    	} else {
+        	i = 0;
+        	container.innerHTML = "";
+   		 }
+		}, 800);
+	};
 	
 
 });
