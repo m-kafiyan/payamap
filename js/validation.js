ac70913fb20baa3a9e933fc0349ef12d18acda67
Mostafa Dashti <mo_dashti1@yahoo.com>
2015-03-06 15:52:36 +0330

    validation

--- js/validation.js
+++ js/validation.js
@@ -1,27 +1,61 @@
-function validateinfoForm() {
-    //نام و نام خانوادگی
+function validateEmail(email) { 
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+} 
 
-    var x = document.forms["myForm"]["name"].value;
-    if (x == null || x == "") {
+function phonenumber(mobile)  
+{  
+  var phoneno = /^09\d{9}$/;  
+	return phoneno.test(mobile);
+} 
+function validateinfoForm() {
+    
+	
+	//نام و نام خانوادگی
+	var name = document.getElementById("name").value;
+    //var x = document.forms["myForm"]["name"].value;
+    if (name.trim() === "") {
         alert("نام را وارد نمایید!");
         return false;
     }
-    if (x.length > 40) {
+	
+	if (name.length > 40) {
         alert("نام: تعداد کاراکتر ها بیشتر از حد مجاز میباشد!");
         return false;
     }
-
-    //ایمیل
-    var x = document.forms["myFormmyForm"]["email"].value;
-    var atpos = x.indexOf("@");
-    var dotpos = x.lastIndexOf(".");
-    if (atpos< 1 || dotpos<atpos+2 || dotpos+2>=x.length) {
-        alert("آدرس ایمیل اشتباه است!");
+	
+	//ایمیل
+    var email = document.getElementById("email").value;
+	if(!validateEmail(email))
+	{	
+		alert("آدرس ایمیل اشتباه است!");
+		return false;
+	}
+	
+	
+    //شماره تماس
+	var mobile = document.getElementById("mobile").value;
+	if(!phonenumber(mobile))
+	{	
+		alert("شماره موبایل وارد شده معتبر نمیباشد!");
+		return false;
+	}
+	
+	
+	//پیام
+	var content = document.getElementById("content").value;
+    if (content.trim() === "") {
+        alert("اطفا پیام خود را وارد نمایید!");
         return false;
     }
-
-    //شماره تماس
-    var x = document.forms["myForm"]["mobile"].value;
+	
+	if (content.length < 10) {
+        alert("پیام وارد شده کمتر از حد مجاز می باشد");
+        return false;
+    }
+	
+    /*
+	var x = document.forms["myForm"]["mobile"].value;
     if (x == null || x == "") {
         alert("شماره تماس را وارد نمایید!");
         return false;
@@ -34,7 +68,9 @@ function validateinfoForm() {
         alert("شماره تماس: لطفا عدد وارد نمایید!");
         return false;
     }
+	*/
 
+	return true;
     
 }
 
