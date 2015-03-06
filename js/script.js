a3f0240ced5e1898181dcb08b42f292c32889894
kafiyan <maedeh.kafiyan@gmail.com>
2015-03-06 13:42:47 +0330

    update

--- js/script.js
+++ js/script.js
@@ -18,9 +18,11 @@ $(document).ready(function(){
 
 		$('.navbar-toggle').click(function() {
 			$('.navbar-toggle').fadeOut();
+			
 			$('.menu').addClass('opened');
 			var height = $( window ).height();
 			$('.menu').css('height',height);
+
 
 	});
 
@@ -43,17 +45,18 @@ $(document).ready(function(){
 		text_arr[1]='مشهد;';
 		text_arr[2]='اصفهان;';
 		text_arr[3]='تبریز;';
-		text_arr[4]='کرج;';
-		text_arr[5]='آمل;';
+		text_arr[4]='آمل;';
+		text_arr[5]='کرج;';
 		text_arr[6]='اراک;';
-		text_arr[7]='اردبیل;';
+		text_arr[7]='کرمانشاه;';
 		text_arr[8]='ارومیه;';
-		text_arr[9]='اهواز;';
-		text_arr[10]='بابل;';
+		text_arr[9]='بابل;';
+		text_arr[10]='اهواز;';
 		text_arr[11]='زنجان;';
 		text_arr[12]='شیراز;';
 		text_arr[13]='کرمان;';
-		text_arr[14]='کرمانشاه;';
+		text_arr[14]='اردبیل;';
+
 
 		var str = text_arr[city_id];
 		text = str.slice(0, ++i);
@@ -73,23 +76,8 @@ $(document).ready(function(){
 		if( char === '>' ) isTag = false;
 
 		if (isTag) return type();
-		setTimeout(type, 600);
+		setTimeout(type, 250);
 	}());
-
-
-	// for (var j=0; j<text.length; j++) {
-	// 	var chars = text[j].slice('');
-	// 	var container = document.getElementById("write");
-	// 	var i = 0;
-	// 	setInterval(function () {
-    // 	if (i < chars.length) {
-    //    	 container.innerHTML += chars[i++];
-    // 	} else {
-    //     	i = 0;
-    //     	container.innerHTML = "";
- //   		 }
-	// 	}, 220);
-	// };
 
 
 });
